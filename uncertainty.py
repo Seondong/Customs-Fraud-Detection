607c2a5ef2f9eefd96146fd3599ead8a7bed020a
Jaechan-So <jcs0510@kaist.ac.kr>
2020-08-07 15:29:47 +0900

    Update uncertainty.py
    - Label name modification (uncertain)
    - Temporary test codes
    - Typo correction

--- uncertainty.py
+++ uncertainty.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import preprocess_data
+import pickle
 from xgboost import XGBClassifier, XGBRegressor
 
 def uncertainty_naive(test, column_to_use_unc_measure) :
@@ -8,12 +9,12 @@ def uncertainty_naive(test, column_to_use_unc_measure) :
 
     uncertainty_coefficient = 1/(len(column_to_use_unc_measure))
 
-    test['uncertainty_score'] = 0
+    test['uncertain'] = 0
 
     for col in column_to_use_unc_measure :
-        test['uncertainty_score'] = test['uncertainty_score'] + uncertainty_coefficient * test['unc.'+col]
+        test['uncertain'] = test['uncertain'] + uncertainty_coefficient * test['unc.'+col]
     
-    return test['uncertainty_score']
+    return test['uncertain']
 
 def scaling(x) :
     if x < 0 :
@@ -24,7 +25,7 @@ def scaling(x) :
 
 def uncertainty_measurement(train, test) :
     # Columns to use
-    numeric_columns = [fob.value', 'cif.value', 'total.taxes', 'gross.weight', 'quantity', 'Unitprice', 'WUnitprice', 'TaxRatio', 'FOBCIFRatio', 'TaxUnitquantity', 'SGD.DayofYear', 'SGD.WeekofYear', 'SGD.MonthofYear']
+    numeric_columns = ['fob.value', 'cif.value', 'total.taxes', 'gross.weight', 'quantity', 'Unitprice', 'WUnitprice', 'TaxRatio', 'FOBCIFRatio', 'TaxUnitquantity', 'SGD.DayofYear', 'SGD.WeekofYear', 'SGD.MonthofYear']
     categorical_columns = [col for col in train.columns if 'RiskH' in col]
     column_to_use_unc_measure = numeric_columns + categorical_columns
 
@@ -36,8 +37,8 @@ def uncertainty_measurement(train, test) :
         columns = [col for col in column_to_use_unc_measure if col != cc]
 
         print("Training xgboost model for predicting %s" % cc)
-        xgb_trainx = pd.DataFrame(train, columns = columns)
-        xgb_testx = pd.DataFrame(test, columns = columns)
+        xgb_trainx = pd.DataFrame(train_unc, columns = columns)
+        xgb_testx = pd.DataFrame(test_unc, columns = columns)
 
         xgb_clf = XGBClassifier(n_estimators=100)
         xgb_clf.fit(xgb_trainx,train[cc].values)
@@ -52,8 +53,8 @@ def uncertainty_measurement(train, test) :
         columns = [col for col in column_to_use_unc_measure if col != nc]
         
         print("Training xgboost model for predicting %s" % nc)
-        xgb_trainx = pd.DataFrame(train, columns=columns)
-        xgb_testx = pd.DataFrame(test, columns=columns)
+        xgb_trainx = pd.DataFrame(train_unc, columns=columns)
+        xgb_testx = pd.DataFrame(test_unc, columns=columns)
         
         # Regression Model
         xgb_reg = XGBRegressor(n_estimators = 100)
@@ -61,7 +62,26 @@ def uncertainty_measurement(train, test) :
         
         xgb_reg_pred = xgb_reg.predict(xgb_testx)
         test['pred.'+nc] = xgb_reg_pred.tolist()
-        test['unc.'+nc] = scaling(abs(test[nc] - test['pred.'+nc]) / test[nc])
+        test['unc.'+nc] = abs(test[nc] - test['pred.'+nc]) / test[nc]
+        test['unc.'+nc] = test['unc.'+nc].apply(lambda x : scaling(x))
         print(test['pred.'+nc].describe())
     
     return uncertainty_naive(test, column_to_use_unc_measure)
+
+# -----------------------------------------------
+# Temporary test codes
+# -----------------------------------------------
+
+# load preprocessed data
+with open("./processed_data.pickle","rb") as f :
+    processed_data = pickle.load(f)
+print(processed_data.keys())
+print("Finish loading data...")
+
+# train/test data 
+train = processed_data["raw"]["train"]
+valid = processed_data["raw"]["valid"]
+test = processed_data["raw"]["test"]
+
+print(uncertainty_measurement(train, test)[1:20])
+print(test.columns)
