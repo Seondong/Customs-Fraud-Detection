28f3a3b7e19af765541a0dac3f98a9841e785c85
John <50077665+john-mai-2605@users.noreply.github.com>
2020-08-13 22:58:54 +0900

    Random sampling (#20)
    * add ramdom sampling
    
    Co-authored-by: nguyenthi47 <ndkthi@kaist.ac.kr>

---
+++ query_strategies/random_sampling.py
@@ -0,0 +1,18 @@
+import numpy as np
+import random
+
+class RandomSampling(Strategy):
+	def __init__(self, model_path, test_loader, args):
+		super(RandomSampling, self).__init__(self, model_path, test_loader, args)
+
+	def query(self, k):
+		num_data = test_loader.dataset.tensors[-1].shape[0]
+		return random.sample(range(0,num_data-1),k)
+
+# class RandomSampling():
+# 	def __init__(self, test_loader):
+# 		self.test_loader = test_loader
+
+# 	def query(self, k):
+# 		num_data = test_loader.dataset.tensors[-1].shape[0]
+# 		return random.sample(range(0,num_data-1),k)
