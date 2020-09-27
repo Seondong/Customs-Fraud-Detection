4174b6571692ac0c915e6622e8ad02922be1c3f6
John <50077665+john-mai-2605@users.noreply.github.com>
2020-09-27 17:56:28 +0900

    Add loss\ (#53)

---
+++ model/utils.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.utils.data as Data
+import numpy as np 
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, logits=False, reduce=True):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.logits = logits
+        self.reduce = reduce
+
+    def forward(self, inputs, targets):
+        if self.logits:
+            BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduce=False)
+        else:
+            BCE_loss = F.binary_cross_entropy(inputs, targets, reduce=False)
+        pt = torch.exp(-BCE_loss)
+        F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
+
+        if self.reduce:
+            return torch.mean(F_loss)
+        else:
+            return F_loss
