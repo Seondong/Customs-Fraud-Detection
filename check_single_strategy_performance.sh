960379e205545b5c86408713672afc81c9fe68cd
Sundong Kim <sdkim0211@gmail.com>
2020-10-15 03:52:23 +0900

    WWW21 exp (#64)
    * Update readme (#56)
    
    * update readme and analysis.ipynb
    
    * check merge opportunity (#58)
    
    * update analysis-week.ipynb
    
    * updated badge to select 100 percent

--- check_single_strategy_performance.sh
+++ check_single_strategy_performance.sh
@@ -1,28 +1,44 @@
-    echo check_single_strategy_performance - M dataset
-    
-    sleep 2
-    
-    
-    python main.py --data real-m --semi_supervised 0 --sampling DATE --mode scratch --train_from 20130101 --test_from 20130701 --test_length 14 --valid_length 14 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 5 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay
-    
-    python main.py --data real-m --semi_supervised 0 --sampling DATE --mode scratch --train_from 20130101 --test_from 20130701 --test_length 14 --valid_length 14 --initial_inspection_rate 100 --final_inspection_rate 4.5 --epoch 5 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay
+sleep 5
+export CUDA_VISIBLE_DEVICES=0 && python main.py --data real-m --semi_supervised 0 --sampling DATE --mode scratch --train_from 20130101 --test_from 20130301 --test_length 7 --valid_length 28 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 10 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay --batch_size 512 &> ./logs/nohup-m1-DATE-5.out & 
 
-    
-    echo check_single_strategy_performance - N dataset
-    
-    sleep 2
-    
-    
-    python main.py --data real-n --semi_supervised 0 --sampling DATE --mode scratch --train_from 20130101 --test_from 20130201 --test_length 7 --valid_length 7 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 5 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay
-    
-    python main.py --data real-n --semi_supervised 0 --sampling DATE --mode scratch --train_from 20130101 --test_from 20130201 --test_length 7 --valid_length 7 --initial_inspection_rate 100 --final_inspection_rate 4.5 --epoch 5 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay
-    
-    
-    echo check_single_strategy_performance - t dataset
-    
-    sleep 2
-    
-    
-    python main.py --data real-t --semi_supervised 0 --sampling DATE --mode scratch --train_from 20150101 --test_from 20150201 --test_length 7 --valid_length 7 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 5 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay
-    
-    python main.py --data real-t --semi_supervised 0 --sampling DATE --mode scratch --train_from 20150101 --test_from 20150201 --test_length 7 --valid_length 7 --initial_inspection_rate 100 --final_inspection_rate 4.5 --epoch 5 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay
+sleep 5
+export CUDA_VISIBLE_DEVICES=2 && python main.py --data real-n --semi_supervised 0 --sampling DATE --mode scratch --train_from 20130101 --test_from 20130301 --test_length 7 --valid_length 28 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 10 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay --batch_size 512 &> ./logs/nohup-n1-DATE-5.out & 
+
+sleep 5
+export CUDA_VISIBLE_DEVICES=3 && python main.py --data real-t --semi_supervised 0 --sampling DATE --mode scratch --train_from 20150101 --test_from 20150301 --test_length 7 --valid_length 28 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 10 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay --batch_size 512 &> ./logs/nohup-t1-DATE-5.out & 
+
+sleep 5
+export CUDA_VISIBLE_DEVICES=0 && python main.py --data real-m --semi_supervised 0 --sampling DATE --mode scratch --train_from 20130101 --test_from 20130301 --test_length 7 --valid_length 28 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 10 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay --batch_size 512 &> ./logs/nohup-m2-DATE-5.out & 
+
+sleep 5
+export CUDA_VISIBLE_DEVICES=1 && python main.py --data real-n --semi_supervised 0 --sampling DATE --mode scratch --train_from 20130101 --test_from 20130301 --test_length 7 --valid_length 28 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 10 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay --batch_size 512 &> ./logs/nohup-n2-DATE-5.out & 
+
+sleep 5
+export CUDA_VISIBLE_DEVICES=2 && python main.py --data real-t --semi_supervised 0 --sampling DATE --mode scratch --train_from 20150101 --test_from 20150301 --test_length 7 --valid_length 28 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 10 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay --batch_size 512 &> ./logs/nohup-t2-DATE-5.out & 
+
+sleep 5
+export CUDA_VISIBLE_DEVICES=3 && python main.py --data real-m --semi_supervised 0 --sampling DATE --mode scratch --train_from 20130101 --test_from 20130301 --test_length 7 --valid_length 28 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 10 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay --batch_size 512 &> ./logs/nohup-m3-DATE-5.out & 
+
+sleep 5
+export CUDA_VISIBLE_DEVICES=0 && python main.py --data real-n --semi_supervised 0 --sampling DATE --mode scratch --train_from 20130101 --test_from 20130301 --test_length 7 --valid_length 28 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 10 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay --batch_size 512 &> ./logs/nohup-n3-DATE-5.out & 
+
+sleep 5
+export CUDA_VISIBLE_DEVICES=1 && python main.py --data real-t --semi_supervised 0 --sampling DATE --mode scratch --train_from 20150101 --test_from 20150301 --test_length 7 --valid_length 28 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 10 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay --batch_size 512 &> ./logs/nohup-t3-DATE-5.out & 
+
+sleep 5
+export CUDA_VISIBLE_DEVICES=2 && python main.py --data real-m --semi_supervised 0 --sampling DATE --mode scratch --train_from 20130101 --test_from 20130301 --test_length 7 --valid_length 28 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 10 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay --batch_size 512 &> ./logs/nohup-m4-DATE-5.out & 
+
+sleep 5
+export CUDA_VISIBLE_DEVICES=3 && python main.py --data real-n --semi_supervised 0 --sampling DATE --mode scratch --train_from 20130101 --test_from 20130301 --test_length 7 --valid_length 28 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 10 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay --batch_size 512 &> ./logs/nohup-n4-DATE-5.out & 
+
+sleep 5
+export CUDA_VISIBLE_DEVICES=0 && python main.py --data real-t --semi_supervised 0 --sampling DATE --mode scratch --train_from 20150101 --test_from 20150301 --test_length 7 --valid_length 28 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 10 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay --batch_size 512 &> ./logs/nohup-t4-DATE-5.out & 
+
+sleep 5
+export CUDA_VISIBLE_DEVICES=1 && python main.py --data real-m --semi_supervised 0 --sampling DATE --mode scratch --train_from 20130101 --test_from 20130301 --test_length 7 --valid_length 28 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 10 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay --batch_size 512 &> ./logs/nohup-m5-DATE-5.out & 
+
+sleep 5
+export CUDA_VISIBLE_DEVICES=2 && python main.py --data real-n --semi_supervised 0 --sampling DATE --mode scratch --train_from 20130101 --test_from 20130301 --test_length 7 --valid_length 28 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 10 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay --batch_size 512 &> ./logs/nohup-n5-DATE-5.out & 
+
+sleep 5
+export CUDA_VISIBLE_DEVICES=3 && python main.py --data real-t --semi_supervised 0 --sampling DATE --mode scratch --train_from 20150101 --test_from 20150301 --test_length 7 --valid_length 28 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 10 --closs bce --rloss full --save 0 --numweeks 300 --inspection_plan direct_decay --batch_size 512 &> ./logs/nohup-t5-DATE-5.out & 
