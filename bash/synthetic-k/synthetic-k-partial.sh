1ab39a567fb880d0b20c4dd4e5df061fd2596b93
Sundong Kim <sdkim0211@gmail.com>
2021-08-03 15:56:38 +0900

    update real-k

---
+++ bash/synthetic-k/synthetic-k-partial.sh
@@ -0,0 +1,13 @@
+python main.py --data synthetic-k-partial --prefix kcsmock --batch_size 128 --sampling random --mode scratch --train_from 20200101 --test_from 20200131 --test_length 7 --valid_length 7 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 2 --closs bce --rloss full --save 0 --numweeks 100 --inspection_plan direct_decay --initial_masking natural;
+
+python main.py --data synthetic-k-partial --prefix kcsmock --batch_size 128 --sampling risky --mode scratch --train_from 20200101 --test_from 20200131 --test_length 7 --valid_length 7 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 2 --closs bce --rloss full --save 0 --numweeks 100 --inspection_plan direct_decay --initial_masking natural --risk_profile ratio;
+
+python main.py --data synthetic-k-partial --prefix kcsmock --batch_size 128 --sampling xgb --mode scratch --train_from 20200101 --test_from 20200131 --test_length 7 --valid_length 7 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 2 --closs bce --rloss full --save 0 --numweeks 100 --inspection_plan direct_decay --initial_masking natural;
+
+python main.py --data synthetic-k-partial --prefix kcsmock --batch_size 128 --sampling DATE --mode scratch --train_from 20200101 --test_from 20200131 --test_length 7 --valid_length 7 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 2 --closs bce --rloss full --save 0 --numweeks 100 --inspection_plan direct_decay --initial_masking natural;
+
+python main.py --data synthetic-k-partial --prefix kcsmock --batch_size 128 --sampling hybrid --subsamplings xgb/random --weights 0.9/0.1 --mode scratch --train_from 20200101 --test_from 20200131 --test_length 7 --valid_length 7 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 2 --closs bce --rloss full --save 0 --numweeks 100 --inspection_plan direct_decay --initial_masking natural --risk_profile ratio;
+
+python main.py --data synthetic-k-partial --prefix kcsmock --batch_size 128 --sampling hybrid --subsamplings DATE/bATE --weights 0.9/0.1 --mode scratch --train_from 20200101 --test_from 20200131 --test_length 7 --valid_length 7 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 2 --closs bce --rloss full --save 0 --numweeks 100 --inspection_plan direct_decay --initial_masking natural --risk_profile ratio;
+
+python main.py --data synthetic-k-partial --prefix kcsmock --batch_size 128 --sampling adahybrid --subsamplings DATE/bATE --weights 0.9/0.1 --mode scratch --train_from 20200101 --test_from 20200131 --test_length 7 --valid_length 7 --initial_inspection_rate 100 --final_inspection_rate 5 --epoch 2 --closs bce --rloss full --save 0 --numweeks 100 --inspection_plan direct_decay --initial_masking natural --risk_profile ratio;
